dc794c0184ae6b7d98d4d30784df35685766f55d
haru <jesuisharu03@gmail.com>
2025-03-23 10:28:10 +0900

    メモの書き込みの不具合解消

--- pages/memo.py
+++ pages/memo.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 st.title("memo")
-st.info("use for todo, memo, other...")
+st.info("use for todo, memo, and any others...")
 
 try:
-        with open("vault.txt", "r", encoding="utf-8") as f:
+        with open("memo.txt", "r", encoding="utf-8") as f:
             st.write(f.read())
 except:
         st.warning("text not found")
@@ -18,6 +18,6 @@ if st.button("save"):
         st.rerun() #ページを再実行
 
 if st.button("delete all"):
-        with open("vault.txt", "w", encoding="utf-8") as f:
+        with open("memo.txt", "w", encoding="utf-8") as f:
             st.write(" ")
         st.rerun()
